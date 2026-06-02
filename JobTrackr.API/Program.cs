44c4fa2347a846f4f5a9f4570e31f31cacd0f380
Fettah <fettah.tonka@gmail.com>
2026-06-02 23:04:39 +0300

    Revise the program.cs

--- JobTrackr.API/Program.cs
+++ JobTrackr.API/Program.cs
@@ -101,10 +101,11 @@ builder.Services.AddCors(options =>
     {
         policy
             .WithOrigins(
-                "http://localhost:5173",
-                "http://127.0.0.1:5173",
-                "http://localhost:8080",
-                "http://127.0.0.1:8080")
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "http://localhost:8080",
+    "http://127.0.0.1:8080",
+    "https://white-rock-071c81c10.7.azurestaticapps.net")
             .AllowAnyHeader()
             .AllowAnyMethod();
     });
